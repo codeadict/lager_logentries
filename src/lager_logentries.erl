fa4c0bfd1deea00fe02242f929dfb1dc4a05d233
Dairon Medina Caro <dairon@gooee.com>
2019-02-19 12:19:20 -0500

    Export types

--- src/lager_logentries.erl
+++ src/lager_logentries.erl
@@ -1,3 +1,10 @@
+-module(lager_logentries).
+
+-export_type([host/0,
+              port_number/0,
+              mask/0,
+              address_family/0,
+              json_object/0]).
 
 -type host() :: inet:hostname().
 -type port_number() :: inet:port_number().
@@ -5,3 +12,5 @@
 
 %% log level mask - definition taken from lager
 -type mask() :: {mask, integer()}.
+
+-type json_object() :: {atom() | binary(), jsx:json_term()}.
